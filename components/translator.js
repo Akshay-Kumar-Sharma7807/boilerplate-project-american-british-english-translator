77272b0f72acf40786b3d4f422e7dfff8eed2ebb
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2021-07-02 12:17:34 +0530

    Making the logic for translator with regex

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,40 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  translate(text) {
+    let string = text.match(/[a-z]+/gi);
+    console.log(string);
+    let letterArray = text.match(/[a-z]/gi);
+    console.log(letterArray);
+    // let string = text.split("");
+    // string[0] = string[0].toUpperCase();
+    // text = string.join("");
+    // console.log(string);
 
+    // let anyChanges = false;
+
+    // var stringArr = text.split(/( )|(\n)/g);
+    // console.log(stringArr);
+
+    // stringArr.map((word, index) => {
+    //   if (americanOnly.hasOwnProperty(word)) {
+    //     console.log("it has property");
+    //     stringArr[index] =
+    //       '<span class="highlight">' + americanOnly[word] + "</span>";
+    //     anyChanges = true;
+    //     console.log(stringArr[index]);
+    //   }
+    // });
+
+    // console.log(stringArr);
+    // if (!anyChanges) {
+    //   return "Everything looks good to me!";
+    // }
+    // return stringArr.join(" ");
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,25 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    console.log(req.body);
+    const { text, locale } = req.body;
+    if (locale === undefined || text === undefined) {
+      res.json({ error: "Required field(s) missing" });
+    }
+    if (text === "") {
+      res.json({ error: "No text to translate" });
+    }
+    if (locale === "american-to-british") {
+      const result = translator.translate(text);
+      res.json({ text, translation: result });
+    } else if (locale === "british-to-american") {
+    } else {
+      res.json({ error: "Invalid value for locale field" });
+    }
+  });
 };
